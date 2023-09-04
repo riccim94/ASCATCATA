ee239e49c17e0aca24423a7b4625e455e4e52c88
riccim94 <62140760+riccim94@users.noreply.github.com>
2023-09-04 11:50:23 +0200

    upload
    upload

--- R/asca_ti.R
+++ R/asca_ti.R
@@ -85,8 +85,8 @@ asca_ti <- function(formula, data, timecol,
     refk <- c("refk")
 
     data2 %>% .[,c(1:2, i)] %>% cbind(data2 %>% .[,3:length(colnames1)] %>%
-                                        .[, names(.) %in% name] %>% as.data.frame(.) %>% ifelse(!is.null(ncol(.)),
-                                                                                                unite(., col = name2, sep = "_"), .)) %>%
+      .[, names(.) %in% name] %>% as.data.frame(.) %>%
+        ifelse(!is.null(ncol(.)), unite(., col = name2, sep = "_"), .)) %>%
       `colnames<-`(c(names(data2)[c(1,2,i)], name2)) -> temp;
     temp[refk] <- temp[,as.character(timecol)]
 
@@ -94,7 +94,7 @@ asca_ti <- function(formula, data, timecol,
       group_by_at(vars(as.name(refk), as.name(name2))) %>% slice(1) %>%
       ungroup() %>%
       pivot_wider(names_from = refk, values_from = as.symbol(anchor)) %>%
-      column_to_rownames(name2) %>%
+      column_to_rownames(name2) %>% .[,-1] %>%
       mutate_all(., function(x){x <- ifelse(is.na(x), mean(x, na.rm = T), x)}) %>%
       mutate_all(., function(x){x <- x - mean(x, na.rm = T)}) %>%
       prcomp()
@@ -105,9 +105,9 @@ asca_ti <- function(formula, data, timecol,
   data3[["info"]][["timecol"]] <- unique(data[,as.character(timecol)])
 
 
-  if(is_tibble(data3[["info"]][["attributes"]])){
-    data3[["info"]][["attributes"]] <- pull(data3[["info"]][["attributes"]])
-  }
+  # if(is_tibble(data3[["info"]][["attributes"]])){
+  #   data3[["info"]][["attributes"]] <- pull(data3[["info"]][["attributes"]])
+  # }
 
   options(contrasts =  prev_contr)
   return(data3)
